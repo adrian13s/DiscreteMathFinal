7960836af91111ecb5515968ebba316dff08b0c5
adrian13s <38772868+adrian13s@users.noreply.github.com>
2018-04-29 11:59:35 -0500

    Update MST.py

--- MST.py
+++ MST.py
@@ -1,7 +1,4 @@
-""" 
-This file should be your main program
-which solves the MST using either
-Prims or Kruskals (bonus for both)
-"""
 from Prims import Prims
+from prims_functions import G
 
+Prims(G)
