d934a874c6f945d2f3fd52fba4d79a14154214fe
adrian13s <38772868+adrian13s@users.noreply.github.com>
2018-04-30 19:39:33 -0500

    Update Prims.py

--- Prims.py
+++ Prims.py
@@ -1,12 +1,3 @@
-"""
-This file should implement Prims
-algorithm 
-"""
-#1.Choose Vertex
-#2.Choose shortest edge of vertex
-#3.Choose nearest vertex not yet in the solution, repeat
-
-#Prims Algorithm
 from prims_functions import cost, min_valid_incident_edge, initial_tree,G
 import prims_functions as P
 
@@ -14,8 +5,10 @@ import prims_functions as P
 V = G.vertex_set()
 E = G.edge_set()
 
-def Prims(Graph):
-    VT = {0} #initial #VT is vertex
+def Prims(Graph,InitialVertex,ShowIterations):
+    
+    
+    VT = {InitialVertex} #initial #VT is vertex
     ET = [] #ET is min edge
     Tree = (VT, ET) #MST
     
@@ -25,19 +18,33 @@ def Prims(Graph):
     new_vertices = {min_edge[0], min_edge[1]}
     cost_min_edge=P.cost(G,P.min_valid_incident_edge(G,Tree))
     
-    """Prims alg"""
-    x=0
-    while Tree[0] != V:
-        x+=1
-        print('Iteration :',x)
-        min_edge = P.min_valid_incident_edge(G,Tree)#
-        new_vertices = {min_edge[0], min_edge[1]}
-        ET.append(min_edge)
-        Tree =[new_vertices.union(Tree[0]),ET]
-        print(Tree)
+    """Prims alg w/ Iterations"""
+    if ShowIterations == "y":
+        x=0
+        while Tree[0] != V:
+            x+=1
+            print('Iteration :',x)
+            min_edge = P.min_valid_incident_edge(G,Tree)#
+            new_vertices = {min_edge[0], min_edge[1]}
+            ET.append(min_edge)
+            Tree =[new_vertices.union(Tree[0]),ET]
+            print("MST:",Tree)
+            print("")
+    
+    
+    elif ShowIterations == "n":
+        while Tree[0] != V:
+            min_edge = P.min_valid_incident_edge(G,Tree)#
+            new_vertices = {min_edge[0], min_edge[1]}
+            ET.append(min_edge)
+            Tree =[new_vertices.union(Tree[0]),ET]
+        print("MST:",Tree)
+        print("")
+   
+        
+#This shows the min cost of the MST        
     total_cost =0
     for e in Tree[0]:
         total_cost += cost_min_edge
     print ("The total cost of the MST is:",total_cost)
-        
         
