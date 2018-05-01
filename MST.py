fe54acd088dad09bc4934108c7214bb7c36c74b0
adrian13s <38772868+adrian13s@users.noreply.github.com>
2018-04-30 19:39:10 -0500

    Update MST.py

--- MST.py
+++ MST.py
@@ -1,4 +1,9 @@
 from Prims import Prims
 from prims_functions import G
 
-Prims(G)
+V = G.vertex_set()
+E = G.edge_set()
+InitialVertex = int(input("What is your initial vertex of these vertices?:"))
+ShowIterations = input("Do want to see the iterations(y/n)?:")
+print("")#extra space
+Prims(G,InitialVertex,ShowIterations)
