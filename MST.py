dc6063c7dfb824de7410bc907f786235183455ca
Estefany Lemus <37642009+estina22@users.noreply.github.com>
2018-04-30 20:08:41 -0500

    Update MST.py

--- MST.py
+++ MST.py
@@ -1,2 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Estefany Lemus
 
+Main program that solves MST
+"""
+
+from Prims import Prims
+from Prims_functions import G
+
+
+V = G.vertex_set()
+E = G.edge_set()
+
+Initial_Vertex = int(input('Enter the vertex where you wish to begin: '))
+Show_Iterations = eval(input('Do want to see the iterations (y/n)?: '))
+
+Prims(G,Initial_Vertex,Show_Iterations, Show_Cost)
 
