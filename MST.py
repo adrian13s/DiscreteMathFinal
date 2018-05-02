32a0ad17bf28b0fb274da3c426b81682dbb93c4a
Estefany Lemus <37642009+estina22@users.noreply.github.com>
2018-05-02 10:53:10 -0500

    Final edit for submission of MST.py

--- MST.py
+++ MST.py
@@ -1,12 +1,14 @@
 #Adrian Saenz
 #Estefany Lemus
-#
+#Ary Hernandez
 
 from prims import Prims
 from prims_functions import G
 
 
-Initial_Vertex = int(input("Enter the vertex where you wish to begin? "))
-Show_Iteration = eval(input("Would you like to see the iterated path (yes/no) ? "))
+Initial_Vertex = int(input('Enter the vertex where you wish to begin: '))
+Show_Iteration = input('Would you like to see the iterated path (yes/no)? :  ')
 
 Prims(G,Initial_Vertex,Show_Iteration)
+
+
