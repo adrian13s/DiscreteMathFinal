482abd503007d4fdff684d13e7e03556dea640f8
adrian13s <38772868+adrian13s@users.noreply.github.com>
2018-05-02 11:36:45 -0500

    Update MST.py

--- MST.py
+++ MST.py
@@ -1,2 +1,15 @@
+#Adrian Saenz
+#Estefany Lemus
+#Ary Hernandez
+
+from prims import Prims
+from prims_functions import G
+
+
+Initial_Vertex = int(input('Enter the vertex where you wish to begin: '))
+Show_Iteration = input('Would you like to see the iterated path (yes/no)? :  ')
+
+Prims(G,Initial_Vertex,Show_Iteration)
+
 
 
